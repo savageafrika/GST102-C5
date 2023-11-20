ab8c37073c66baa0667aecffe5ef4161341c86d1
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:02:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,323 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "__ is a group of sentences or single sometimes that forms a meaningful unit",
+
+         answer : "Paragraph",
+
+         options : [
+
+                          "Clause",
+
+                          "Paragraph",
+
+                          "Phrase",
+
+                          "Sentence"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "Which of these is not a component of paragraph",
+
+         answer : "Introductory sentence",
+
+         options : [
+
+                          "Introductory sentence",
+
+                          "Topic sentence",
+
+                          "Supporting details",
+
+                          "Concluding sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "___ introduces the main idea",
+
+         answer : "Topic sentence",
+
+         options : [
+
+                          "Introductory sentence",
+
+                          "Topic sentence",
+
+                          "Supporting details",
+
+                          "Concluding sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "The steps to writing a good paragraph include these except",
+
+         answer : "The organising stage",
+
+         options : [
+
+                          "The prewriting stage",
+
+                          "The publishing stage",
+
+                          "The organising stage",
+
+                          "The writing stage"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "At which stage of paragraph writing does the writer carefully organise his ideas",
+
+         answer : "The prewriting stage",
+
+         options : [
+
+                          "The prewriting stage",
+
+                          "The publishing stage",
+
+                          "The organising stage",
+
+                          "The writing stage"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "The stage of thinking what to write is",
+
+         answer : "Brainstorming",
+
+         options : [
+
+                          "Drafting",
+
+                          "Brainstorming",
+
+                          "Research",
+
+                          "Organising"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "__ involves writing down your own ideas quickly without worrying about being perfect or staying on topic",
+
+         answer : "Drafting",
+
+         options : [
+
+                          "Drafting",
+
+                          "Prosetry",
+
+                          "Research",
+
+                          "Crafting"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "Effective paragraphs have five main characteristics. Which of these not among them",
+
+         answer : "Punctuation",
+
+         options : [
+
+                          "Unity",
+
+                          "Adequate development",
+
+                          "Punctuation",
+
+                          "Topic sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "___ refers to the extent to which all ideas contained within a given paragraph harmoniously relate",
+
+         answer : "Unity",
+
+         options : [
+
+                          "Unity",
+
+                          "Adequate development",
+
+                          "Punctuation",
+
+                          "Topic sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "In paragraph writing, coherence rests on__",
+
+         answer : "Logical progression",
+
+         options : [
+
+                          "Unity",
+
+                          "Adequate development",
+
+                          "Logical progression",
+
+                          "Variety"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "An effective device for ensuring unity in a paragraph is",
+
+         answer : "Topic sentence",
+
+         options : [
+
+                          "Variety",
+
+                          "Adequate development",
+
+                          "Coherence",
+
+                          "Topic sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "Words that specify relationship between sentences and between paragraphs are called",
+
+         answer : "Transitional words",
+
+         options : [
+
+                          "Linking verbs",
+
+                          "Verbal bridges",
+
+                          "Subordinators",
+
+                          "Transitional words"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "Adding sentence variety to ___ can give it to life and rhythm",
+
+         answer : "Prose",
+
+         options : [
+
+                          "Sentence",
+
+                          "Prose",
+
+                          "Poetry",
+
+                          "Paragraph"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
